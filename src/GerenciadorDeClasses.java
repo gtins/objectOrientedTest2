bb00fa910ae073a0b977bb2266cc309e6c5d2fb7
Gustavo <gu.martins@catolicasc.edu.br>
2023-05-22 23:51:23 -0300

    90%

--- src/GerenciadorDeClasses.java
+++ src/GerenciadorDeClasses.java
@@ -1,21 +1,24 @@
+import java.io.*;
 import java.util.HashMap;
+
 public class GerenciadorDeClasses {
     HashMap<Integer, String> contatos = new HashMap<>();
 
     public void addContato(int numero, String nome) {
         contatos.put(numero, nome);
+        System.out.println("Contato Adicionado: " + nome + " - " + numero);
     }
 
     public void remContato(String nome){
+        System.out.println("Removendo Contato...");
         for (HashMap.Entry<Integer, String> entry : contatos.entrySet()) { //para a entrada do hashmap, que recebe intger e string, para cada entryset de contatos...
             if (entry.getValue().equals(nome)) {//se o valor da entrada equivaler ao nome (parametro do metodo)...
                 contatos.remove(entry.getKey());//remove
+                System.out.println("Contato Removido: " + nome);
                 break; //fim do loop
             }
         }
     }
-
-
     public void showContatos() {
         for (String nome : contatos.values()) {
             System.out.println(nome);
@@ -28,5 +31,47 @@ public class GerenciadorDeClasses {
             }
         }
         return -404;
+    }
+
+    public void gravar(String arquivo){
+        try {
+
+            FileOutputStream gravado = new FileOutputStream(arquivo);
+            ObjectOutputStream saida = new ObjectOutputStream(gravado);
+
+            saida.writeObject(contatos);
+
+            System.out.println("Contatos gravados em: " + arquivo);
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+    public void leitura(String arquivo){
+        try {
+
+            FileInputStream lendo =  new FileInputStream(arquivo);
+            ObjectInputStream entrada = new ObjectInputStream(lendo);
+
+            contatos = (HashMap<Integer, String>) entrada.readObject(); // cast? (intelij corrigiu)
+
+            System.out.println("Contatos lidos de: " + arquivo);
+            parseTexto();
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado: " + e.getMessage());
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+    public String parseTexto() {
+
+        StringBuilder text = new StringBuilder();
+
+        for (Integer numero : contatos.keySet()) { // para ( para cada numero no keyset de contatos) nome recebe o getter do numero de contatos
+            String nome = contatos.get(numero);
+            text.append(nome).append(" - ").append(numero).append("\n");
+        }
+        return text.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,46 @@
 public class Main {
     public static void main(String[] args) {
         GerenciadorDeClasses aplicativo = new GerenciadorDeClasses();
-        aplicativo.addContato(3,"kleber");
-        aplicativo.addContato(4,"pedro");
-        aplicativo.addContato(5,"maria");
-        aplicativo.addContato(8,"rosa");
-        aplicativo.remContato("kleber");
+        System.out.println("-----------------------");
+
+        aplicativo.addContato(99998888,"Kleber");
+        aplicativo.addContato(88887777,"Pedro");
+        aplicativo.addContato(77776666,"Maria");
+        aplicativo.addContato(66665555,"Rosa");
+
+        System.out.println("-----------------------");
+
+        aplicativo.remContato("Kleber");
+
+        System.out.println("-----------------------");
+
+        aplicativo.gravar("teste");
+        aplicativo.contatos.clear();
+        aplicativo.leitura("teste");
+
+        System.out.println("-----------------------");
+
         aplicativo.showContatos();
-        int numero = aplicativo.searchContato("rosa");
-        System.out.println(numero);
+
+        System.out.println("-----------------------");
+
+        String contatosTexto = aplicativo.parseTexto();
+        System.out.println(contatosTexto);
+
+        System.out.println("-----------------------");
+
+        int numeroJoao = aplicativo.searchContato("João");
+        if (numeroJoao != -404) {
+            System.out.println("Número do João: " + numeroJoao);
+        } else {
+            System.out.println("Contato não encontrado: João");
+        }
+
+        int numeroPedro = aplicativo.searchContato("Pedro");
+        if (numeroPedro != -404) {
+            System.out.println("Número do Pedro: " + numeroPedro);
+        } else {
+            System.out.println("Contato não encontrado: pedro");
+        }
     }
 }
